9f4cc079772b3baf7eeb840a700a40cdac1d5aa4
NicholasKang <nicholas.kang@kingmakergames.co>
2021-05-07 14:32:46 +0800

    branch created feat: read from external data source

--- src/pages/TransactionHistory.js
+++ src/pages/TransactionHistory.js
@@ -1,11 +1,19 @@
 import { Button, Card, Col, DatePicker, Row, Space, Table } from "antd";
 import React from "react";
 import "./TransactionHistory.css";
+import { tmpDataBase } from "../transactionPlaceHolderData.js";
+
+const userInfo = tmpDataBase[0];
 
 const { RangePicker } = DatePicker;
 
 function TransactionHistory() {
 	const columns = [
+		{
+			title: "PayeeID",
+			dataIndex: "payeeID",
+			key: "payeeID",
+		},
 		{
 			title: "Date",
 			dataIndex: "date",
@@ -15,17 +23,7 @@ function TransactionHistory() {
 			title: "Amount",
 			dataIndex: "amount",
 			key: "amount",
-		},
-		{
-			title: "Status",
-			dataIndex: "status",
-			key: "status",
-		},
-		{
-			title: "Reference ID",
-			dataIndex: "refid",
-			key: "refid",
-		},
+		}
 	];
 
 	const data = [
@@ -35,8 +33,13 @@ function TransactionHistory() {
 			status: "Received",
 			refid: "001",
 		},
+		{
+			date: "12/04/2021",
+			amount: "S$50",
+			status: "Received",
+			refid: "001",
+		}
 	];
-
 	return (
 		<div>
 			<Space>
@@ -44,14 +47,14 @@ function TransactionHistory() {
 					<Row gutter={16}>
 						<Col span={8}>
 							<Card title="Balance" bordered={false}>
-								S$12
+								{userInfo.balance}
 							</Card>
 						</Col>
 					</Row>
 					Date Range:
 					<RangePicker />
 					<Button type="primary">Search</Button>
-					<Table columns={columns} dataSource={data} />
+					<Table columns={columns} dataSource={userInfo.transactions} />
 				</div>
 			</Space>
 		</div>
---
+++ src/transactionPlaceHolderData.js
@@ -0,0 +1,140 @@
+export const tmpDataBase = [
+  {
+    "custid": "115550f4-635d-4880-a814-805edd848225",
+    "balance": 722,
+    "transactions": [
+      {
+        "payeeID": "0963f296-72d4-4862-8cf4-dc1088aaf086",
+        "date": "09-08-2019-19-52-19",
+        "Time": "13-33-22",
+        "amount": 1330.48,
+        "eGift": false,
+        "message": "Consequat exercitation velit ex excepteur id."
+      },
+      {
+        "payeeID": "a7445a7b-4e00-4cdc-a7ae-445d21cf8850",
+        "date": "18-06-2013-18-24-43",
+        "Time": "20-40-39",
+        "amount": 1873.8,
+        "eGift": true,
+        "message": "Deserunt nisi esse quis sunt cupidatat sit nostrud exercitation exercitation."
+      },
+      {
+        "payeeID": "89d960c1-f87a-49d3-b4a3-3886b9257942",
+        "date": "07-08-2014-3-48-9",
+        "Time": "3-39-23",
+        "amount": 1234.83,
+        "eGift": false,
+        "message": "Non tempor ullamco occaecat do pariatur ut est velit cupidatat culpa."
+      },
+      {
+        "payeeID": "1c2cb474-675f-40de-ae54-d5ac6d9ec999",
+        "date": "27-04-2017-11-27-3",
+        "Time": "22-11-46",
+        "amount": 1648.21,
+        "eGift": false,
+        "message": "Nostrud ullamco esse cillum esse fugiat elit nulla aliqua."
+      }
+    ]
+  },
+  {
+    "custid": "cda05671-dce4-4df2-a46b-4866e35224a3",
+    "balance": 1903,
+    "transactions": [
+      {
+        "payeeID": "f6d2fbc0-4796-4cfb-8b5d-8a8ec8fdc069",
+        "date": "28-11-2005-23-8-43",
+        "Time": "10-6-43",
+        "amount": 607.21,
+        "eGift": false,
+        "message": "Culpa veniam consectetur enim ad cupidatat."
+      },
+      {
+        "payeeID": "4a71f1e3-4e69-4560-8b33-2b35110d4518",
+        "date": "29-07-2015-0-22-20",
+        "Time": "2-2-29",
+        "amount": 843.68,
+        "eGift": true,
+        "message": "Do ullamco aliqua sint tempor cillum pariatur ipsum occaecat consequat do ea Lorem."
+      },
+      {
+        "payeeID": "8c7fa0ce-98e6-4b78-ac75-32a3d4ea244e",
+        "date": "20-05-2004-11-17-20",
+        "Time": "17-33-22",
+        "amount": 933.48,
+        "eGift": false,
+        "message": "Pariatur incididunt pariatur non Lorem et dolor cillum."
+      },
+      {
+        "payeeID": "66faf2a5-65c5-4bae-8ecf-f88ea8b09df7",
+        "date": "30-04-2015-23-21-25",
+        "Time": "20-52-36",
+        "amount": 78.63,
+        "eGift": false,
+        "message": "Cupidatat ea cillum est eu officia minim laborum."
+      },
+      {
+        "payeeID": "fea758b1-9447-4110-a4a0-dd2274c8f401",
+        "date": "28-07-2010-15-52-17",
+        "Time": "9-21-7",
+        "amount": 167.68,
+        "eGift": true,
+        "message": "Exercitation officia ex ullamco voluptate fugiat officia ipsum eiusmod excepteur."
+      }
+    ]
+  },
+  {
+    "custid": "aa911170-85af-40fe-9724-73e6c1b89695",
+    "balance": 1893,
+    "transactions": [
+      {
+        "payeeID": "924387ac-6ea2-4ede-a9d2-d2d86f65ec8b",
+        "date": "02-05-2001-11-11-35",
+        "Time": "9-19-22",
+        "amount": 1042.75,
+        "eGift": false,
+        "message": "Anim culpa elit magna nulla qui excepteur excepteur aliquip."
+      },
+      {
+        "payeeID": "40129be4-47e8-4322-8273-7b15dc6b2b45",
+        "date": "25-12-2018-15-33-50",
+        "Time": "6-25-55",
+        "amount": 1298.25,
+        "eGift": true,
+        "message": "Ullamco nulla ullamco consectetur ullamco culpa consequat ullamco mollit ea dolor mollit Lorem in."
+      },
+      {
+        "payeeID": "62c01e74-74dd-4218-b505-a0254b8a335e",
+        "date": "01-08-2008-2-6-14",
+        "Time": "3-5-50",
+        "amount": 886.21,
+        "eGift": true,
+        "message": "Pariatur velit ad esse aliquip Lorem exercitation."
+      },
+      {
+        "payeeID": "7a4509de-c4b4-4064-b672-6d16fcdc9ce9",
+        "date": "26-08-2000-18-46-22",
+        "Time": "17-49-26",
+        "amount": 1706.28,
+        "eGift": false,
+        "message": "Dolore sunt consequat adipisicing tempor occaecat non consectetur ullamco ex duis ullamco esse."
+      },
+      {
+        "payeeID": "154da526-e066-4610-be14-643c7b19c8f9",
+        "date": "24-10-2002-14-21-0",
+        "Time": "4-45-57",
+        "amount": 1439.83,
+        "eGift": false,
+        "message": "Officia ipsum in Lorem tempor minim fugiat."
+      },
+      {
+        "payeeID": "8b58c3ea-7c22-4edb-ab1b-6618db6c0d37",
+        "date": "23-11-2019-17-50-2",
+        "Time": "9-20-54",
+        "amount": 698.08,
+        "eGift": false,
+        "message": "Cupidatat cillum mollit quis duis nisi occaecat duis tempor minim irure ullamco minim ut."
+      }
+    ]
+  }
+];
